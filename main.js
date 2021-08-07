af6ea82c613e9bb9af75cc21e3f4ca7a5f60e5cc
Daniel Wang <daniel@campaignagent.com.au>
2021-08-07 21:05:40 +1000

    feat: add react component code image

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-
-
-
 const step = 1000;
 var rightBtn = document.querySelector('.scroller-right-btn');
 var leftBtn = document.querySelector('.scroller-left-btn');
@@ -31,17 +28,24 @@ function sideWayScroll(direction) {
   }, 700);
 };
 
+
+const container = document.querySelector(".container");
+
+container.addEventListener("wheel", (evt) => {
+  var xOffset = window.pageXOffset;
+  disableBtns(xOffset);
+});
+
 function disableBtns(scrollDis){
   var ww = window.innerWidth;
   var xOffset = window.pageXOffset;
-  var container = document.querySelector('.container');
   var cw = container.scrollWidth;
   var t = scrollDis + ww * 2;
-  console.log(ww + xOffset);
-  console.log('offset: ' + xOffset + ' cw: ' + cw);
+  // console.log(ww + xOffset);
+  // console.log('offset: ' + xOffset + ' cw: ' + cw);
   if (xOffset == 0){
       leftBtn.disabled = true;
-      console.log('start');
+    //  console.log('start');
   } else if (ww + xOffset == cw){
       rightBtn.disabled = true;
   } else {
